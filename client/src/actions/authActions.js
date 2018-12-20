8a26582e135da0d2f24a78184cb2c943959b8333
John Pachkoski <jpachkoski@gmail.com>
2018-12-19 20:07:46 -0500

    Initial express server with route files

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,6 +4,7 @@ import jwt_decode from 'jwt-decode';
 
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
+// Make all of these action creators asynchronous, later.
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
